af8204a2d9273a62056bde274a0efec3b2fe492f
Yang Lin <yang.lin@hy-tech.com.tw>
2019-08-13 16:27:17 +0800

    測試綁定門號

---
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/bean/Person.java
@@ -0,0 +1,28 @@
+package com.example.bot.spring.bean;
+
+public class Person {
+
+	private String lineId;
+	private String name;
+	private String isSetting;
+	public String getLineId() {
+		return lineId;
+	}
+	public void setLineId(String lineId) {
+		this.lineId = lineId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIsSetting() {
+		return isSetting;
+	}
+	public void setIsSetting(String isSetting) {
+		this.isSetting = isSetting;
+	}
+	
+	
+}
--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -16,9 +16,13 @@
 
 package com.example.bot.spring.echo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.bot.spring.bean.Person;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
@@ -30,6 +34,14 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 @SpringBootApplication
 @LineMessageHandler
 public class EchoApplication {
+	
+	
+	private static Map<String,Person> personMap;
+	
+	static{
+		personMap = new HashMap<String,Person>();
+	}
+	
     public static void main(String[] args) {
         SpringApplication.run(EchoApplication.class, args);
     }
@@ -39,12 +51,62 @@ public class EchoApplication {
         System.out.println("event: " + event);
         final String originalMessageText = event.getMessage().getText();
         
+        String lineId = event.getSource().getUserId();
+        Person person = null;
+        if(lineId != null && !lineId.isEmpty()){
+        	person = getPersonById(lineId);
+        }
+        
+        boolean isSetting = false;
+        if(person != null ){
+        	isSetting = "Y".equals(person.getIsSetting());
+        }
+        
         TextMessage result = null;
+        
         if("87".equals(originalMessageText)){
-        	result =  new TextMessage("你全家都 88");
+        	result =  new TextMessage("你全家都 87");
         	
+        }else if("[HY]綁定ID".equals(originalMessageText)){
+        	if(person != null){
+        		person.setIsSetting("Y");
+        	}else{
+        		person = new Person();
+        		person.setLineId(lineId);
+        		person.setIsSetting("Y");
+        	}
+        	
+        	boolean isSuccess = savePerson(person);
+        	
+        	if(isSuccess){
+        		result =  new TextMessage("請輸入要綁定的名稱");
+        	}else{
+        		result =  new TextMessage("系統錯誤，請再試一次");
+        	}
+        	
+        }else if(isSetting && originalMessageText != null){
+        	
+        	if(person == null){
+        		result =  new TextMessage("系統錯誤，請再試一次");
+        	}else{
+        		person.setName(originalMessageText);;
+        		
+        		boolean isSuccess = savePerson(person);
+        		
+        		if(isSuccess){
+        			result =  new TextMessage(String.format("已為您綁定的名稱 : %s", originalMessageText));
+        		}else{
+        			result =  new TextMessage("系統錯誤，請再試一次");
+        		}
+        	}
+        	
+        	
+        }else if("[HY]查詢名稱".equals(originalMessageText)){
+        	result =  new TextMessage(String.format("您的名子為 : %s", person.getName()));
+        }else if("[HY]查詢ID".equals(originalMessageText)){
+        	result =  new TextMessage(String.format("您的 ID 為 : %s", person.getLineId()));
         }else{
-        	result =  new TextMessage(originalMessageText);
+        	result =  new TextMessage(String.format("這個我看不懂喔 : %s", originalMessageText));
         }
         return result;
     }
@@ -53,4 +115,32 @@ public class EchoApplication {
     public void handleDefaultMessageEvent(Event event) {
         System.out.println("event: " + event);
     }
+    
+    private boolean savePerson(Person person){
+    	
+    	boolean result = false;
+    	
+    	
+    	try {
+    		personMap.put(person.getLineId(), person);
+			result = true;
+			
+			return result;
+		} catch (Exception e) {
+			return false;
+		}
+
+    }
+    
+	private Person getPersonById(String lineId) {
+
+		Person person = null;
+		
+		try {
+			person = personMap.get(lineId);
+			return person;
+		} catch (Exception e) {
+			return person;
+		}
+	}
 }
